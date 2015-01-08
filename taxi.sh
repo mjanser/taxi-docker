4b3ea8eb14dcced03ecaed43c644225f30ab1125
Martin Janser <martin@duss-janser.ch>
2015-01-08 11:28:15 +0100

    Set SELinux context for files if SELinux is available and enabled

--- taxi.sh
+++ taxi.sh
@@ -4,6 +4,10 @@ TKSRC=$HOME/.tksrc
 CONFIGDIR=$HOME/.taxi
 DATADIR=$(eval echo $(grep "^file = " $TKSRC | cut -d" " -f3 | cut -d% -f1))
 
+if [ $(command -v selinuxenabled >/dev/null 2>&1) -a $(selinuxenabled) ]; then
+    chcon -Rt svirt_sandbox_file_t $TKSRC $CONFIGDIR $DATADIR
+fi
+
 docker run \
     --rm \
     -i \
